596a4510654aad4c2d1124842e1c2739e02fabe3
Ian Zimmerman <75636013+IanZimmyman@users.noreply.github.com>
2021-03-03 01:52:19 -0500

    Create Lab5_Figures | figures.hpp

---
+++ Lab5_Figures | figures.hpp
@@ -0,0 +1,17 @@
+//prototypes the functions found in figures.cpp
+//Ian Zimmerman
+//Feb 19, 2021
+
+//came with the file, leaving in comments since im using the ifndef thing
+//#pragma once
+
+#ifndef FIGURES_HPP
+#define FIGURES_HPP
+
+int filledSquare(int); //prints a filled square, inputing size from figuresInput.cpp
+int hollowSquare(int); //prints a hollow square, inputing size from figuresInput.cpp
+int slash(int); //prints a forward slash, inputing size from figuresInput.cpp
+int backslash(int); //prints a backslash, inputing size from figuresInput.cpp
+int diamond(int); //prints a hollow diamond, inputing size from figuresInput.cpp
+
+#endif
